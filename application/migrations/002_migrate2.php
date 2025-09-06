7ad8cb445c03a4a625c8feaab068ba217abf90e2
firman1616 <firmanfirdaus542@gmail.com>
2025-09-06 21:43:47 +0700

    update migrasi level 2

--- application/migrations/002_migrate2.php
+++ application/migrations/002_migrate2.php
@@ -30,7 +30,7 @@ class Migration_Migrate2 extends CI_Migration
                 'constraint' => '100',
                 'null'       => FALSE
             ],
-             'tgl_lahir' => [
+            'tgl_lahir' => [
                 'type'       => 'DATE',
                 'null'       => FALSE
             ],
@@ -64,7 +64,7 @@ class Migration_Migrate2 extends CI_Migration
                 'type'       => 'text',
                 'null'       => FALSE
             ],
-             'filestempel' => [
+            'filestempel' => [
                 'type'       => 'text',
                 'null'       => FALSE
             ],
@@ -72,6 +72,47 @@ class Migration_Migrate2 extends CI_Migration
         $this->dbforge->add_key('id', TRUE);
         $this->dbforge->create_table('tbl_paspor', TRUE);
 
+        // insert modul
+        $this->db->insert('tbl_modul', [
+            'nama'      => 'Paspor',
+            'icon'      => 'fas fa-passport',
+            'url_modul' => 'Paspor'
+        ]);
+        $modul_id = $this->db->insert_id();
+
+        // insert user
+        $this->db->insert('tbl_user', [
+            'name'       => 'User Demo',
+            'username'   => 'demo',
+            'password'   => password_hash('demo', PASSWORD_BCRYPT), // hash password
+            'status'     => 1,
+            'nik'        => '12345',
+            'created_at' => date('Y-m-d H:i:s')
+        ]);
+        $user_id = $this->db->insert_id();
+
+        // insert role
+        $this->db->insert('tbl_role', [
+            'nama_role' => 'Demo'
+        ]);
+        $role_id = $this->db->insert_id();
+
+        // insert role akses (1–9)
+        $dataAkses = [];
+        for ($i = 1; $i <= 9; $i++) {
+            $dataAkses[] = [
+                'akses'   => $i,
+                'role_id' => $role_id
+            ];
+        }
+        $this->db->insert_batch('tbl_role_akses', $dataAkses);
+
+        // insert user modul akses
+        $this->db->insert('tbl_user_modul_akses', [
+            'user_id'  => $user_id,
+            'modul_id' => $modul_id,
+            'role_id'  => $role_id
+        ]);
     }
 
     public function down()
